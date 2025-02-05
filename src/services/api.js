40c2e8e0de59daf48127ebdaef7def24c8251238
Eniola Samuel <theeniolasamuel@gmail.com>
2025-02-05 15:39:35 +0100

    subsequent commit

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api'; // Adjust this to your backend URL
+
+export const getAllApartments = () => axios.get(`${API_URL}/apartments/all`);
+export const bookApartment = (bookingData) => axios.post(`${API_URL}/bookings/book`, bookingData);
+export const adminRegisterApartment = ()=> axios.post(`${API_URL}/apartments/register`);

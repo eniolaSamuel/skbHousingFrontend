b262ccf829808ce1638ff96c26511bce3265d0d5
Eniola Samuel <theeniolasamuel@gmail.com>
2025-02-06 17:01:41 +0100

    subsequent commit

--- src/services/api.js
+++ src/services/api.js
@@ -6,4 +6,4 @@ export const getAllApartments = () => axios.get(`${API_URL}/apartments/all`);
 export const bookApartment = (bookingData) => axios.post(`${API_URL}/bookings/book`, bookingData);
 export const adminRegisterApartment = ()=> axios.post(`${API_URL}/apartments/register`);
 export const getApartmentsByType = (type) => axios.get(`${API_URL}/filter/type`);
-export const
+export const getApartmentsByLocation = (location) => axios.get(`${API_URL}/filter/location`);

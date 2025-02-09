cd8f1b37223cb7e418d5ccb65e3a11a0ede15fca
Eniola Samuel <theeniolasamuel@gmail.com>
2025-02-09 23:37:54 +0100

    subsequent commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import logo from "../assets/logo.png" // Make sure to add your logo file
+import logo from "../assets/SkbLogo.svg"
 
 const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false)
